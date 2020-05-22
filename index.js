5a6db55f67350594daae8ce36b4dc9ee3b848ea9
Márk Diószegi <walnut5588@gmail.com>
2020-05-22 23:37:40 +0200

    first task completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 let usersDivEl;
 let postsDivEl;
+//let comments 
 let loadButtonEl;
 
 function createPostsList(posts) {
@@ -18,9 +19,31 @@ function createPostsList(posts) {
         pEl.appendChild(strongEl);
         pEl.appendChild(document.createTextNode(`: ${post.body}`));
 
+        //data post id
+        const dataPostIdAttr = document.createAttribute("data-post-id");
+        dataPostIdAttr.value = post.id;
+
+        /* //data comment id
+        const dataCommentIdAttr = document.createAttribute("data-comment-id");
+        dataCommentIdAttr.value = post.id; */
+
         // creating list item
         const liEl = document.createElement('li');
+        //liEl.setAttributeNode(dataPostIdAttr);
         liEl.appendChild(pEl);
+
+        //create the button then append it
+        const buttonEl = document.createElement("button");
+        buttonEl.textContent = "See related comments";
+        buttonEl.setAttributeNode(dataPostIdAttr);
+        buttonEl.addEventListener("click", onLoadComments);
+        liEl.appendChild(buttonEl);
+
+        /* //create a div so we can reach this to put comments in
+        const divEl = document.createElement("div");
+        divEl.id = "comments-" + post.id;
+        liEl.append(divEl);
+        //-- */
 
         ulEl.appendChild(liEl);
     }
@@ -44,6 +67,18 @@ function onPostsReceived() {
 function onLoadPosts() {
     const el = this;
     const userId = el.getAttribute('data-user-id');
+
+    //remove active from everything first
+    const buttonElements = document.querySelectorAll("[data-user-id]");
+    buttonElements.forEach(button => {
+        if (button != el) {
+            button.classList.remove("active");
+        } else {
+            el.classList.add("active");
+        }
+    });
+
+    //make active
 
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', onPostsReceived);
@@ -122,6 +157,79 @@ function onLoadUsers() {
     xhr.addEventListener('load', onUsersReceived);
     xhr.open('GET', BASE_URL + '/users');
     xhr.send();
+}
+
+function onLoadComments() {
+    const postId = this.getAttribute("data-post-id");
+
+    //console.log("wtf is this", this.parentNode);
+    //toggle active
+    const buttonElements = document.querySelectorAll("[data-post-id]");
+    buttonElements.forEach(button => {
+        if (button != this) {
+            button.classList.remove("active");
+        } else {
+            this.classList.add("active");
+        }
+    });
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", onCommentsReceived);
+    xhr.open("GET", BASE_URL + "/comments?postId=" + postId);
+    xhr.send();
+    //console.log("loaded!")
+}
+
+function onCommentsReceived() {
+    const text = this.responseText;
+    const comments = JSON.parse(text);
+
+    /* console.log(comments);
+    console.log(comments[0].postId); */
+
+    const buttonElements = document.querySelectorAll("[data-post-id]");
+
+    buttonElements.forEach(e => {
+        if (e.parentNode.querySelector("ul") != null)
+            e.parentNode.querySelector("ul").remove();
+
+        if (e.getAttribute("data-post-id") == comments[0].postId) {
+            //console.log(e.parentNode);
+            e.parentNode.appendChild(createComments(comments));
+        }
+    });
+
+    //create comments
+    //console.log();
+}
+
+function createComments(comments) {
+    const ulEl = document.createElement("ul");
+
+    for (let i = 0; i < comments.length; i++) {
+        const comment = comments[i]
+
+        //data comment id
+        const dataCommentIdAttr = document.createAttribute("data-comment-id");
+        dataCommentIdAttr.value = comment.id;
+
+        const liEl = document.createElement("li");
+        liEl.setAttributeNode(dataCommentIdAttr);
+        ulEl.appendChild(liEl);
+
+        //Name
+        const strongEl = document.createElement("strong");
+        strongEl.textContent = comment.name;
+
+        //Body text
+        const pEl = document.createElement("p");
+        pEl.textContent = comment.body;
+
+        pEl.appendChild(strongEl);
+        liEl.appendChild(pEl);
+    }
+
+    return ulEl;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
